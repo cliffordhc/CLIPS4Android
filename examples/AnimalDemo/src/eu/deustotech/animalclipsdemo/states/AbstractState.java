1b10db1a0301f6461883bbe2d8c8ef9a5c2f44aa
cliffordc <cliffordhc@gmail.com>
2025-05-03 00:01:34 +0200

    Deleted old files

--- examples/AnimalDemo/src/eu/deustotech/animalclipsdemo/states/AbstractState.java
+++
@@ -1,4 +0,0 @@
-package eu.deustotech.animalclipsdemo.states;
-
-
-public abstract class AbstractState {}
--- examples/AnimalDemo/src/eu/deustotech/animalclipsdemo/states/NextStateListener.java
+++
@@ -1,7 +0,0 @@
-package eu.deustotech.animalclipsdemo.states;
-
-public interface NextStateListener {
-	public void started(InitialState state);
-	public void nextState(UsualState state);
-	public void finished(FinalState state);
-}
